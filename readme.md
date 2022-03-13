5d863e1c577b25e693394fff7c916fce16d8b2f9
vrossello <vrossello@travelcompositor.com>
2022-03-13 22:16:29 +0100

    Selenium4 parallel tests compared to selenium 3

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Instructions:
 
-Setup selenium images:
+Setup selenium4:
 
 `docker-compose up -d`
 
@@ -8,22 +8,4 @@ Run tests:
 
 `./gradlew test`
 
-Output looks like:
-
-`[INFO ] 2021-11-04 09:50:59.690 [Test worker] DriverCreationTest - Selenium 3 Opened in 622 ms
-nov 04, 2021 9:50:59 A. M. org.openqa.selenium.remote.ProtocolHandshake createSession
-INFO: Detected dialect: W3C
-[INFO ] 2021-11-04 09:50:59.851 [Test worker] DriverCreationTest - Selenium 3 Opened in 97 ms
-nov 04, 2021 9:51:00 A. M. org.openqa.selenium.remote.ProtocolHandshake createSession
-INFO: Detected dialect: W3C
-[INFO ] 2021-11-04 09:51:00.005 [Test worker] DriverCreationTest - Selenium 3 Opened in 94 ms
-nov 04, 2021 9:51:01 A. M. org.openqa.selenium.remote.ProtocolHandshake createSession
-INFO: Detected dialect: W3C
-[INFO ] 2021-11-04 09:51:01.723 [Test worker] DriverCreationTest - Selenium 4 Opened in 1652 ms
-nov 04, 2021 9:51:06 A. M. org.openqa.selenium.remote.ProtocolHandshake createSession
-INFO: Detected dialect: W3C
-[INFO ] 2021-11-04 09:51:06.715 [Test worker] DriverCreationTest - Selenium 4 Opened in 4918 ms
-nov 04, 2021 9:51:11 A. M. org.openqa.selenium.remote.ProtocolHandshake createSession
-INFO: Detected dialect: W3C
-[INFO ] 2021-11-04 09:51:11.718 [Test worker] DriverCreationTest - Selenium 4 Opened in 4929 ms
-`
+The selenium3 test ends and the selenium4 one does not end.
--- src/test/java/org/example/DriverCreationTest.java
+++ src/test/java/org/example/DriverCreationTest.java
@@ -6,36 +6,55 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class DriverCreationTest {
     private static final Logger LOGGER = LogManager.getLogger(DriverCreationTest.class);
-
+    private final static int TASKS = 30;
 
     @Test
-    public void selenium3_hub() throws MalformedURLException {
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-        for (var i = 0; i < 3; i++) {
-            Date now =new Date();
-            var driver = new RemoteWebDriver(new URL("http://localhost:5444/wd/hub/"), options);
-            LOGGER.info("Selenium 3 Opened in {} ms", new Date().getTime() - now.getTime());
-            driver.quit();
+    public void selenium4() throws ExecutionException, InterruptedException {
+        parallel_test("http://localhost:4444/");
+    }
+
+    @Test
+    public void selenium3() throws ExecutionException, InterruptedException {
+        parallel_test("http://localhost:6444/wd/hub");
+    }
+
+
+
+    public void parallel_test(String host) throws InterruptedException, ExecutionException {
+        var executor = Executors.newFixedThreadPool(TASKS);
+        var results = new ArrayList<Future>();
+        for (var i = 0; i < TASKS; i++) {
+            results.add(executor.submit(() -> openDriver(host)));
+        }
+        for (var result : results) {
+            result.get();
         }
     }
 
 
-    @Test
-    public void selenium4_hub() throws MalformedURLException {
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-        for (var i = 0; i < 3; i++) {
-            Date now =new Date();
-            var driver = new RemoteWebDriver(new URL("http://localhost:4444/"), options);
-            LOGGER.info("Selenium 4 Opened in {} ms", new Date().getTime() - now.getTime());
+    private void openDriver(String host) {
+        try {
+            Date now = new Date();
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);
+            var driver = new RemoteWebDriver(new URL(host), options);
+            for (var i = 0; i < 10; i++) {
+                driver.get("https://ifconfig.co");
+            }
+            LOGGER.info("Driver opened in {} ms", new Date().getTime() - now.getTime());
             driver.quit();
+        } catch (Exception e) {
+            LOGGER.error("Error", e);
+            throw new RuntimeException(e);
         }
     }
 
